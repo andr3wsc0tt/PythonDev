fdbd5a104e5cbc05d18472c84d1c4bca0a711ad4
Your Name <you@example.com>
2020-06-29 10:28:51 -0600

    update

--- BuiltIns.py
+++ BuiltIns.py
@@ -483,3 +483,8 @@ obj = object()
 print(dir(obj)) # ['__class__', '__delattr__', '__dir__', '__doc__', '__eq__', '__format__', 
 #'__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', 
 # '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__']
+
+# oct(x) : convert an int to an octal string prefixed with "0o".
+
+print(oct(8)) # '0o10'
+print(oct(-56)) # '-0o70
